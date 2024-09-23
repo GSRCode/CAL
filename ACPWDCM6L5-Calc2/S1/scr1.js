46d5547bd37f88f886e8ab3d99ff036fc854782e
Srinivasa Rao <sriteltmpgm1978@gmail.com>
2024-09-23 20:26:19 +0530

    Cool web pages using HTML/CSS/JS

---
+++ ACPWDCM6L5-Calc2/S1/scr1.js
@@ -0,0 +1,32 @@
+function getHistory(){
+	return document.getElementById("history-value").innerText;
+}
+function printHistory(num){
+	document.getElementById("history-value").innerText=num;
+}
+function getOutput(){
+	return document.getElementById("output-value").innerText;
+}
+function printOutput(num){
+	if(num==""){
+		document.getElementById("output-value").innerText=num;
+	}
+	else{
+		document.getElementById("output-value").innerText=getFormattedNumber(num);
+	}	
+}
+function reverseNumberFormat(num){
+	return Number(num.replace(/,/g,''));
+}
+
+
+
+
+function getFormattedNumber(num){
+	if(num=="-"){
+		return "";
+	}
+	var n = Number(num);
+	var value = n.toLocaleString("en");
+	return value;
+}
